5c8b0b4ba64ff29661a54fd8a27ae448443a24a1
Juan Jesus <Juan Jesus@DESKTOP-NAPCKLJ>
2023-06-27 10:40:17 +0200

    Relacion 5 - Matrices

---
+++ src/Rel5_Matrices/Ej1_Rel5.java
@@ -0,0 +1,10 @@
+package Rel5_Matrices;
+
+public class Ej1_Rel5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Juan Jesus
+ *
+ */
+module Relacion5 {
+}
